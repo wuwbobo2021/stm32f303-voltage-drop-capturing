1319e78580fa3148e007d2387ff6bb2a05d926cc
wu bobo <wuwbobo@outlook.com>
2022-02-22 19:28:15 +0800

    ADC dual mode config; fixed bug of 10ms delay before capturing.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// by wuwbobo2021 <https://github.com/wuwbobo2021>, <wuwbobo@outlook.com>
+//by wuwbobo2021 <https://github.com/wuwbobo2021>, <wuwbobo@outlook.com>
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -16,9 +16,7 @@ float ad_vrefint = 1.2 * 4095.0 / 3.3; //it will be updated
 #define GPIO_Pin_ADC GPIO_Pin_1
 #define ADC_SampleTime_Default ADC_SampleTime_7Cycles5
 #define ADC_Diff_Tol 96 //for distinguishing of connected and floating states
-#define ADC_Channel_Bat ADC_Channel_2
-#define ADC_Channel_ADC1 ADC_Channel_Bat
-#define ADC_Channel_ADC2 ADC_Channel_18
+#define ADC_Channel_Bat ADC_Channel_2 //PA1
 
 #define VBatMin 0.8
 #define VDecMin 0.05
@@ -39,11 +37,9 @@ typedef enum {
 
 volatile Measure_State st = Measure_Disconnected;
 
-volatile uint32_t TickCount = 0; //SysTick
-volatile uint32_t TimingDelay;
+volatile uint32_t TickCount = 0, TimingDelay = 0; //SysTick
 
-volatile uint16_t buf[ADC_Buffer_Size];
-volatile uint16_t buf_ref[ADC_Buffer_Size];
+volatile uint16_t buf[2 * ADC_Buffer_Size];
 volatile float av[ADC_Output_Cnt];
 
 // Raw data will be broken by this function.
@@ -159,141 +155,79 @@ void adc_gpio_init(void)
 	GPIO_Init(GPIO_ADC, &GPIO_InitStruct);
 }
 
-void adc_dma_init(ADC_TypeDef* ADCx, uint16_t* dma_buf, uint16_t buf_size)
+void adc_calibrate(ADC_TypeDef* ADCx)
 {
-	DMA_Channel_TypeDef* DMA_Channel; uint32_t DMA_IT_TC1;
-	if (ADCx == ADC1) {
-		DMA_Channel = DMA1_Channel1; DMA_IT_TC1 = DMA1_IT_TC1;
-	} else if (ADCx == ADC2) {
-		DMA_Channel = DMA2_Channel1; DMA_IT_TC1 = DMA2_IT_TC1;
-	} else return;
-	
-	RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div6); //12MHz
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12 | RCC_AHBPeriph_DMA1 | RCC_AHBPeriph_DMA2, ENABLE);
-	
 	ADC_VoltageRegulatorCmd(ADCx, ENABLE);
 	Delay(10);
 	ADC_SelectCalibrationMode(ADCx, ADC_CalibrationMode_Single);
 	ADC_StartCalibration(ADCx);
 	while (ADC_GetCalibrationStatus(ADCx) != RESET);
-	ADC_GetCalibrationValue(ADCx);
-	
-	ADC_CommonInitTypeDef ADC_CommonInitStruct;
-	ADC_CommonStructInit(&ADC_CommonInitStruct);
-	ADC_CommonInitStruct.ADC_Mode = ADC_Mode_Independent;
-	ADC_CommonInitStruct.ADC_Clock = ADC_Clock_AsynClkMode; //PLL->Prescaler->ADC, not from AHB HCLK
-	ADC_CommonInitStruct.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
-	ADC_CommonInitStruct.ADC_TwoSamplingDelay = 0;
-	ADC_CommonInit(ADCx, &ADC_CommonInitStruct);
-	
-	ADC_InitTypeDef ADC_InitStruct;
-	ADC_StructInit(&ADC_InitStruct);
-	ADC_InitStruct.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Enable; //continuous mode
-	ADC_InitStruct.ADC_Resolution = ADC_Resolution_12b;
-	ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
-	ADC_InitStruct.ADC_AutoInjMode = ADC_AutoInjec_Disable;
-	ADC_InitStruct.ADC_OverrunMode = ADC_OverrunMode_Enable;
-	ADC_InitStruct.ADC_NbrOfRegChannel = 1;
-	ADC_Init(ADCx, &ADC_InitStruct);
-	
-	if (ADCx == ADC2) ADC_VrefintCmd(ADCx, ENABLE); //18 VRefInt
-	ADC_DMACmd(ADCx, ENABLE);
-	ADC_Cmd(ADCx, ENABLE);
-	while (! ADC_GetFlagStatus(ADCx, ADC_FLAG_RDY));
-	
-	DMA_DeInit(DMA_Channel);
-	DMA_InitTypeDef DMA_InitStruct;
-	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t) &(ADCx->DR);
-	DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t) dma_buf;
-	DMA_InitStruct.DMA_BufferSize = buf_size;
-	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralSRC;
-	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
-	DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-	DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-	DMA_InitStruct.DMA_Mode = DMA_Mode_Normal;
-	DMA_InitStruct.DMA_M2M = DMA_M2M_Disable;
-	DMA_InitStruct.DMA_Priority = DMA_Priority_High;
-	DMA_Init(DMA_Channel, &DMA_InitStruct);
 }
 
-static inline float adc_read(ADC_TypeDef* ADCx)
+static inline void adc_start(void)
 {
-	while ((ADCx->ISR & ADC_FLAG_EOC) == RESET);
-	return ADCx->DR;
+	if ((ADC1->CR & ADC_CR_ADSTART) == RESET) {
+		ADC_StartConversion(ADC1); //ADC2 starts simultaneously
+		
+		while ((ADC1->ISR & ADC_FLAG_EOC) == RESET || (ADC2->ISR & ADC_FLAG_EOC) == RESET);
+		ADC1->DR; ADC2->DR; //discard first data
+	}
 }
 
-static inline void adc_start(ADC_TypeDef* ADCx)
+static inline void adc_prepare_for_dma(void)
 {
-	ADC_StartConversion(ADCx);
-	ADC_ClearFlag(ADCx, ADC_FLAG_OVR);
-}
-
-static inline void adc_stop(ADC_TypeDef* ADCx)
-{
-	ADC_StopConversion(ADCx); while (ADCx->CR & ADC_CR_ADSTP);
-}
-
-//time cost is above 7 ms under 12 MHz ADC Frequency, 7Cycles5 sample time
-float adc_read_average(ADC_TypeDef* ADCx)
-{
-	DMA_Channel_TypeDef* DMA_Channel; uint32_t DMA_IT_TC1;
-	if (ADCx == ADC1) {
-		DMA_Channel = DMA1_Channel1; DMA_IT_TC1 = DMA1_IT_TC1;
-	} else if (ADCx == ADC2) {
-		DMA_Channel = DMA2_Channel1; DMA_IT_TC1 = DMA2_IT_TC1;
-	} else return 0;
-	
-	DMA_SetCurrDataCounter(DMA_Channel, ADC_Buffer_Size); //manual counter reload
-	DMA_Cmd(DMA_Channel, ENABLE);
-	adc_start(ADCx);
-	
-	while (DMA_GetITStatus(DMA_IT_TC1) == RESET); //wait for transfer complete event
-	
-	adc_stop(ADCx);
-	DMA_Cmd(DMA_Channel, DISABLE);
-	DMA_ClearITPendingBit(DMA_IT_TC1);
-	
-	uint16_t* pdata = (ADCx == ADC1)? buf : buf_ref;
-	float sum = 0;
-	for (uint16_t i = 0; i < ADC_Output_Cnt; i++)
-		sum += get_average(pdata + i * ADC_Av_Cnt, ADC_Av_Cnt, ADC_Diff_Tol);
-	return sum / ADC_Output_Cnt;
-}
-
-void adc_overall_init(void)
-{
-	adc_gpio_init();
-	ADC_DeInit(ADC1); //Reset ADC12
-	adc_dma_init(ADC1, buf, ADC_Buffer_Size);
-	adc_dma_init(ADC2, buf_ref, ADC_Buffer_Size);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_ADC1, 1, ADC_SampleTime_Default);
-	ADC_RegularChannelConfig(ADC2, ADC_Channel_ADC2, 1, ADC_SampleTime_Default);
-	ad_vrefint = adc_read_average(ADC2);
-}
-
-void adc_overall_start(void)
-{
-	DMA_SetCurrDataCounter(DMA1_Channel1, ADC_Buffer_Size);
-	DMA_SetCurrDataCounter(DMA2_Channel1, ADC_Buffer_Size);
-	if ((ADC1->CR & ADC_CR_ADSTART) == RESET) ADC_StartConversion(ADC1);
-	if ((ADC2->CR & ADC_CR_ADSTART) == RESET) ADC_StartConversion(ADC2);
-	Delay(10);
-	DMA1_Channel1->CCR |= DMA_CCR_EN; DMA2_Channel1->CCR |= DMA_CCR_EN;
 	ADC1->ISR = ADC_FLAG_OVR; ADC2->ISR = ADC_FLAG_OVR; //actually clears the flag (rc_w1)
 }
 
-bool adc_overall_complete(void)
+static inline void adc_stop(void)
 {
-	return (DMA_GetITStatus(DMA1_IT_TC1) != RESET && DMA_GetITStatus(DMA2_IT_TC1) != RESET);
+	if ((ADC1->CR & ADC_CR_ADSTART) != RESET) return;
+	ADC_StopConversion(ADC1); while (ADC1->CR & ADC_CR_ADSTP); //ADC2 stops simultaneously
 }
 
-void adc_overall_stop(void)
+void adc_dma_capture(void) //time cost is about 7 ms under 12 MHz ADC Frequency, 7Cycles5 sample time
 {
-	DMA_Cmd(DMA1_Channel1, DISABLE); DMA_Cmd(DMA2_Channel1, DISABLE);
-	DMA_ClearITPendingBit(DMA1_IT_TC1); DMA_ClearITPendingBit(DMA2_IT_TC1);
-	if (ADC1->CR & ADC_CR_ADSTART) adc_stop(ADC1);
-	if (ADC2->CR & ADC_CR_ADSTART) adc_stop(ADC2);
+	adc_start();
+	DMA_SetCurrDataCounter(DMA1_Channel1, ADC_Buffer_Size);
+	DMA_Cmd(DMA1_Channel1, ENABLE);
+	adc_prepare_for_dma();
+	
+	while (DMA_GetITStatus(DMA1_IT_TC1) == RESET); //wait for transfer complete event
+	
+	adc_stop();
+	DMA_Cmd(DMA1_Channel1, DISABLE);
+	DMA_ClearITPendingBit(DMA1_IT_TC1);
+}
+
+void adc_get_vrefint(void)
+{
+	adc_dma_capture();
+	for (uint16_t i = 0; i < ADC_Buffer_Size; i++)
+		buf[i] = buf[2*i + 1];
+	ad_vrefint = get_average(buf, ADC_Buffer_Size, ADC_Diff_Tol);
+}
+
+static inline float adc_read(void) //when DMA is not running
+{
+	while ((ADC1->ISR & ADC_FLAG_EOC) == RESET || (ADC2->ISR & ADC_FLAG_EOC) == RESET);
+	uint32_t rdual = ADC1_2->CDR;
+	uint16_t rbat = rdual & 0xFFFF, rref = (rdual >> 16) & 0xFFFF;
+	return (float)rbat * ad_vrefint / rref;
+}
+
+float adc_read_average(void)
+{
+	adc_dma_capture();
+	
+	for (uint16_t i = 0; i < ADC_Buffer_Size; i++)
+		buf[i] = (uint16_t)((float)buf[2*i] * ad_vrefint / buf[2*i + 1]);
+	
+	float sum = 0;
+	for (uint16_t i = 0; i < ADC_Output_Cnt; i++) {
+		av[i] = get_average(buf + i*ADC_Av_Cnt, ADC_Av_Cnt, ADC_Diff_Tol);
+		sum += av[i];
+	}
+	return sum / ADC_Output_Cnt;
 }
 
 static inline float adc_to_voltage(float ad_val)
@@ -301,21 +235,23 @@ static inline float adc_to_voltage(float ad_val)
 	return VRefInt * ad_val/ad_vrefint;
 }
 
-float adc_get_freq(void)
+float adc_get_freq(void) //cost 0.5 s
 {
 	uint32_t cnt = 0;
 	TickCount = 0; //SysTick
 	while (TickCount < 500) {
+		adc_start();
 		DMA_SetCurrDataCounter(DMA1_Channel1, ADC_Buffer_Size);
 		DMA_Cmd(DMA1_Channel1, ENABLE);
-		adc_start(ADC1);
+		adc_prepare_for_dma();
 		
 		while (DMA_GetITStatus(DMA1_IT_TC1) == RESET)
 			if (TickCount >= 500) break;
 		
-		adc_stop(ADC1);
+		adc_stop();
 		cnt += ADC_Buffer_Size - DMA_GetCurrDataCounter(DMA1_Channel1);
-		DMA_Cmd(DMA1_Channel1, DISABLE); DMA_ClearITPendingBit(DMA1_IT_TC1);
+		DMA_Cmd(DMA1_Channel1, DISABLE);
+		DMA_ClearITPendingBit(DMA1_IT_TC1);
 	}
 	
 	return (float)cnt / 500.0; //kHz
@@ -323,13 +259,10 @@ float adc_get_freq(void)
 
 void adc_get_accuracy(uint8_t adc_sampletime, float* acc_worst, float* acc_av)
 {
-	adc_overall_start(); while (! adc_overall_complete()); adc_overall_stop();
-	
-	for (uint16_t i = 0; i < ADC_Buffer_Size; i++)
-		buf[i] = (uint16_t)((float)buf[i] * ad_vrefint / buf_ref[i]);
-	
-	for (uint16_t i = 0; i < ADC_Output_Cnt; i++)
-		av[i] = get_average(buf + i * ADC_Av_Cnt, ADC_Av_Cnt, ADC_Diff_Tol);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_Bat, 1, adc_sampletime);
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_18, 1, adc_sampletime);
+	adc_start(); Delay(10);
+	adc_read_average();
 	
 	// fill in empty items corresponding with data discarded by get_average(), ADC_Diff_Tol.
 	// these zero outputs have no influrence on accuracy, don't pass them to get_diff().
@@ -345,15 +278,69 @@ void adc_get_accuracy(uint8_t adc_sampletime, float* acc_worst, float* acc_av)
 		i++;
 	}
 	
-	*acc_worst = 12.0 - log(get_diff_max(av, 16)) / log(2);
-	*acc_av = 12.0 - log(get_diff_average(av, 16)) / log(2);
+	*acc_worst = 12.0 - log(get_diff_max(av, ADC_Output_Cnt)) / log(2);
+	*acc_av = 12.0 - log(get_diff_average(av, ADC_Output_Cnt)) / log(2);
+}
+
+void adc_dma_init()
+{
+	adc_gpio_init();
+	
+	ADC_DeInit(ADC1); //Reset ADC12
+	RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div6); //12MHz
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12 | RCC_AHBPeriph_DMA1, ENABLE);
+	
+	adc_calibrate(ADC1); adc_calibrate(ADC2);
+	
+	ADC_CommonInitTypeDef ADC_CommonInitStruct;
+	ADC_CommonStructInit(&ADC_CommonInitStruct);
+	ADC_CommonInitStruct.ADC_Mode = ADC_Mode_RegSimul; //ADC Dual Mode, regular simultaneous mode
+	ADC_CommonInitStruct.ADC_Clock = ADC_Clock_AsynClkMode; //PLL->Prescaler->ADC, not from AHB HCLK
+	ADC_CommonInitStruct.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+	ADC_CommonInitStruct.ADC_TwoSamplingDelay = 0;
+	ADC_CommonInit(ADC1, &ADC_CommonInitStruct); //ADC1_2
+	
+	ADC_InitTypeDef ADC_InitStruct;
+	ADC_StructInit(&ADC_InitStruct);
+	ADC_InitStruct.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Enable; //continuous mode
+	ADC_InitStruct.ADC_Resolution = ADC_Resolution_12b;
+	ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStruct.ADC_AutoInjMode = ADC_AutoInjec_Disable;
+	ADC_InitStruct.ADC_OverrunMode = ADC_OverrunMode_Enable; //don't preserve the old data
+	ADC_InitStruct.ADC_NbrOfRegChannel = 1;
+	ADC_Init(ADC1, &ADC_InitStruct); ADC_Init(ADC2, &ADC_InitStruct);
+	
+	ADC_VrefintCmd(ADC2, ENABLE); //VRefInt:Channel18
+	
+	ADC_DMACmd(ADC1, ENABLE);
+	ADC_Cmd(ADC1, ENABLE); ADC_Cmd(ADC2, ENABLE);
+	while (! (ADC_GetFlagStatus(ADC1, ADC_FLAG_RDY) && ADC_GetFlagStatus(ADC2, ADC_FLAG_RDY)));
+	
+	DMA_DeInit(DMA1_Channel1);
+	DMA_InitTypeDef DMA_InitStruct;
+	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t) &(ADC1_2->CDR);
+	DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t) buf;
+	DMA_InitStruct.DMA_BufferSize = ADC_Buffer_Size;
+	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralSRC;
+	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Word;
+	DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;
+	DMA_InitStruct.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStruct.DMA_M2M = DMA_M2M_Disable;
+	DMA_InitStruct.DMA_Priority = DMA_Priority_High;
+	DMA_Init(DMA1_Channel1, &DMA_InitStruct);
+	
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_Bat, 1, ADC_SampleTime_Default);
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_18, 1, ADC_SampleTime_Default);
+	adc_get_vrefint();
 }
 
 int main(void)
 {
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	SysTick_Init();
-	adc_overall_init();
+	adc_dma_init();
 	
 	float freq = adc_get_freq();
 	printf("Current ADC Frequency: %d kHz.\n", (uint16_t) freq);
@@ -368,10 +355,10 @@ int main(void)
 	while (true) {
 		switch (st) {
 			case Measure_Disconnected:
-				ad_vbat = adc_read_average(ADC1);
+				ad_vbat = adc_read_average();
 				if (ad_vbat >= AD_BatMin) {
 					Delay(300);
-					ad_vbat = adc_read_average(ADC1);
+					ad_vbat = adc_read_average();
 					if (ad_vbat < AD_BatMin) break;
 					
 					st = Measure_Connected;
@@ -382,12 +369,12 @@ int main(void)
 					adc_get_accuracy(ADC_SampleTime_Default, &acc_worst, &acc_av);
 					printf("ADC Accuracy: %.1f ~ %.1f Bits.\n\n", acc_worst, acc_av);
 					
-					adc_start(ADC1); adc_read(ADC1);
+					adc_start();
 				}
 				break;
 			
 			case Measure_Connected:
-				ad_curr = adc_read(ADC1);
+				ad_curr = adc_read();
 				
 				if (ad_curr < AD_BatMin) {
 					dis_cnt++; dec_cnt = 0;
@@ -397,9 +384,8 @@ int main(void)
 					}
 				} else if (ad_curr < ad_vbat - AD_DecMin) {
 					dec_cnt++; dis_cnt = 0;
-					if (dec_cnt >= Dec_Cnt_Min) { //start DMA for capturing
+					if (dec_cnt >= Dec_Cnt_Min) { //start capturing
 						st = Measure_Capturing; dec_cnt = 0;
-						adc_overall_start();
 					}
 				} else
 					dis_cnt = dec_cnt = 0;
@@ -407,52 +393,45 @@ int main(void)
 				break;
 
 			case Measure_Capturing:
-				if (! adc_overall_complete()) break;
-				adc_overall_stop(); st = Measure_Later;
-				
-				for (uint16_t i = 0; i < ADC_Buffer_Size; i++)
-					buf[i] = (uint16_t)((float)buf[i] * ad_vrefint / buf_ref[i]);
-				
-				for (uint16_t i = 0; i < ADC_Output_Cnt; i++)
-					av[i] = adc_to_voltage(get_average(buf + i * ADC_Av_Cnt, ADC_Av_Cnt, ADC_Diff_Tol));
+				adc_read_average(); st = Measure_Later;
 				
 				vbat_dec = 0;
 				for (uint16_t i = 1; i < ADC_Output_Cnt; i++) {
 					if (av[i] > 0) {
-						vbat_dec = av[i]; break;
+						vbat_dec = adc_to_voltage(av[i]); break;
 					}
 				}
 				if (vbat - vbat_dec < VDecMin) { //false trigger
-					st = Measure_Connected; adc_start(ADC1); adc_read(ADC1);
+					st = Measure_Connected; adc_start();
 					break;
 				} else if (vbat_dec < VBatMin) { //disconnect event
 					if (av[ADC_Output_Cnt - 1] < VBatMin) {
 						st = Measure_Disconnected;
 						printf("Disconnected.\n\n");
 					} else {
-						st = Measure_Connected; adc_start(ADC1); adc_read(ADC1);
+						st = Measure_Connected; adc_start();
 					}
 					break;
 				}
 				
 				printf("Captured: %.3f V. Voltage Drop: %.3f V\n", vbat_dec, vbat - vbat_dec); Delay(5);
 				for (uint16_t i = 0; i < ADC_Output_Cnt; i++) {
-					printf("%.4f ", av[i]); Delay(5);
+					printf("%.4f ", adc_to_voltage(av[i])); Delay(5);
 				}
 				printf("\n\n");
 				break;
 			
 			case Measure_Later:
-				ad_curr = adc_read_average(ADC1);
+				ad_curr = adc_read_average();
 				if (ad_vbat - ad_curr < AD_DecMin / 4) {
 					st = Measure_Connected; dis_cnt = 0;
-					Delay(1000);
-					ad_vbat = adc_read_average(ADC1);
+					Delay(2000);
+					ad_vbat = adc_read_average();
 					vbat = adc_to_voltage(ad_vbat);
 					printf("Battery Voltage: %.3f V.\n\n", vbat);
 					
-					adc_start(ADC1); adc_read(ADC1);
-				} else if (vbat_dec < VBatMin) {
+					adc_start();
+				} else if (ad_curr < AD_BatMin) {
 					dis_cnt++;
 					if (dis_cnt >= 3) {
 						st = Measure_Disconnected; dis_cnt = 0;
